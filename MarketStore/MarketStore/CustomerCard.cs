2e839323cb4ae8f5513ee9de48eb39d8e9e8cdf9
igorvasic92 <67714685+igorvasic92@users.noreply.github.com>
2020-07-02 09:17:42 +0200

    Add files via upload

---
+++ MarketStore/MarketStore/CustomerCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketStore
+{
+    public class CustomerCard
+    {
+        public int CustomerCardId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Turnover { get; set; }
+        public IDiscountCardType CardInfo { get; set; }
+        public double PurchasePrice { get; set; }
+        public double DiscountRate => CardInfo.DiscountRate(Turnover);
+        public double DiscountRateInPercentage => CardInfo.DiscountCalculator.DiscountRateInPercentage(CardInfo, Turnover);
+        public double TotalDiscount => CardInfo.DiscountCalculator.TotalDiscount(CardInfo, PurchasePrice, Turnover);
+        public double TotalPriceWithDiscount => CardInfo.DiscountCalculator.TotalPriceWithDiscount(CardInfo, PurchasePrice, Turnover);
+    }
+}
